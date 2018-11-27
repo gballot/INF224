3e0481d50b66f04b5714389a856d664fc6d8740c
Gabriel Ballot <gabriel.ballot@telecom-paristech.fr>
2018-11-27 14:38:22 +0100

    WIP étape 1

---
+++ media.h
@@ -0,0 +1,26 @@
+#ifndef MEDIA_H
+#define MEDIA_H
+
+#include <string>
+
+
+class Media {
+    private :
+        std::string name;
+        std::string path;
+    public :
+        Media(std::string name, std::string path) : name(name), path(path) {}
+        Media() {}
+        ~Media() {}
+
+        void setName(std::string name) { this->name = name; }
+        std::string getName() const { return name; }
+
+        void setPath(std::string path) { this-> path = path; }
+        std::string getPath() const { return path; }
+
+        void print
+};
+
+
+#endif
