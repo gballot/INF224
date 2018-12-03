d5d782e1578c628dbe6ec5cfec98b41bbd2dd7f7
Gabriel Ballot <gabriel.ballot@telecom-paristech.fr>
2018-12-03 16:20:41 +0100

    fin 4

--- main.cpp
+++ main.cpp
@@ -17,5 +17,8 @@ int main(/*int argc, const char* argv[]*/)
     media->print(cout);
     Photo * photo = new Photo("photo", "photo1.jpeg", 12, 13);
     photo->print(cout);
+    photo->open();
+    Video * video = new Video("video", "video1.mov", 2.4);
+    video->open();
     return 0;
 }
